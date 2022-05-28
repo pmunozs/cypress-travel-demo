43f3794aab3612c1213f18cd449f2e1c335b8dd1
pmunozs <pablo.munoz.sommaruga@gmail.com>
2022-05-28 08:56:10 +0200

    Error message if e-mail already exists

--- cypress/integration/Signup.js
+++ cypress/integration/Signup.js
@@ -1,24 +1,39 @@
 import Signup from '../support/pages/Signup';
+import { faker } from '@faker-js/faker';
 
 describe('Signup', () => {
 
     const signup = new Signup();
-    
-    before(() => {
-        cy.intercept('/signup').as('Signup')
-        signup.visit()
+
+    beforeEach(() => {
+        cy.intercept('/signup').as('Signup');
+        signup.visit();
         cy.wait('@Signup').its('response.statusCode').should('eq', 200);
     })
 
     it('User should be able to create customer profile', () => {
-        signup.getName().type('John');
-        signup.getLastName().type('Doe');
-        signup.getTelephone().type('12345678');
-        signup.getEmail().type('john.doe.2@e-mail.com');
-        signup.getPassword().type('Masked1!');
+        signup.getName().type(faker.name.firstName());
+        signup.getLastName().type(faker.name.lastName());
+        signup.getTelephone().type(faker.phone.phoneNumber('+1 ########'));
+        signup.getEmail().type(faker.internet.email('qa+', 'demo'));
+        signup.getPassword().type(faker.internet.password(8));
         signup.submit().click();
         cy.wait('@Signup').its('response.statusCode').should('eq', 302);
         cy.get('.alert-success.signup').should('be.visible');
         cy.url().should('contain', 'login/signup');
     })
+
+    it.only('Display error message if e-mail already exists', () => {
+        cy.intercept('**/failed').as('Failed');
+        signup.getName().type(faker.name.firstName());
+        signup.getLastName().type(faker.name.lastName());
+        signup.getTelephone().type(faker.phone.phoneNumber('+1 ########'));
+        signup.getEmail().type('user@phptravels.com');
+        signup.getPassword().type(faker.internet.password(8));
+        signup.submit().click();
+        cy.wait('@Signup').its('response.statusCode').should('eq', 302);
+        cy.wait('@Failed').its('response.statusCode').should('eq', 200);
+        cy.get('.alert-danger').should('be.visible');
+        cy.url().should('contain', 'signup/failed');
+    });
 })
